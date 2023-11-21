ae532da77063dccab1c7ee9d54db24c0b469ceff
Gleb Timoshenko <42435850+GTimoshenko@users.noreply.github.com>
2023-11-22 00:07:09 +0300

    Update UserController.java

--- src/main/java/b2bapp/b2bappbackend/controller/UserController.java
+++ src/main/java/b2bapp/b2bappbackend/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
     private final UserService userService;
 
